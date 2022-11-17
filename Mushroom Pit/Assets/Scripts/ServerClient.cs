5ea2afa35ed7d3eae19a14e11bb60066182cf4b4
Alex Gesti - Pixel Cuak - Mr.Luigi001 <40181553+alexgesti@users.noreply.github.com>
2022-11-17 22:24:17 +0100

    UDP_Basic_UI_Rework

--- Mushroom Pit/Assets/Scripts/ServerClient.cs
+++ Mushroom Pit/Assets/Scripts/ServerClient.cs
@@ -6,6 +6,7 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading;
+using UnityEngine.UI;
 
 public class ServerClient : MonoBehaviour
 {
@@ -17,6 +18,13 @@ public class ServerClient : MonoBehaviour
     Thread thread;
     Thread threadClient;
 
+    // Canvas Connection
+    public InputField userName, serverIP, serverPort, enterMessage;
+    public Text chatBox;
+    string textSend;
+    public Button createB, joinB;
+    enum Profile { server, client }; Profile profile;
+
     // Server
     Socket newsock;
 
@@ -26,10 +34,46 @@ public class ServerClient : MonoBehaviour
     // temporal
     string input, stringData;
 
+    private void Awake()
+    {
+        serverIP.interactable = false;
+    }
+
+    public void ChangeProfile(int prof)
+    {
+        profile = (Profile)prof;
+
+        if (profile == Profile.server)
+        {
+            serverIP.interactable = false;
+            createB.interactable = true;
+            joinB.interactable = false;
+        }
+        else if (profile == Profile.client)
+        {
+            serverIP.interactable = true;
+            createB.interactable = false;
+            joinB.interactable = true;
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return)) textSend = enterMessage.text;
+
+        if (textSend != "")
+        {
+            chatBox.text += textSend + "\n";
+            textSend = "";
+        }
+    }
+
+    /*----- HOST/SERVER -----*/
+
     public void Server()
     {
         data = new byte[1024];
-        ipep = new IPEndPoint(IPAddress.Any, 8194);
+        ipep = new IPEndPoint(IPAddress.Any, int.Parse(serverPort.text));
         newsock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
 
         newsock.Bind(ipep);
@@ -62,10 +106,12 @@ public class ServerClient : MonoBehaviour
         }
     }
 
+    /*----- CLIENT -----*/
+
     public void Client()
     {
         data = new byte[1024];
-        ipep = new IPEndPoint(IPAddress.Parse("10.0.203.43"), 8194);
+        ipep = new IPEndPoint(IPAddress.Parse(serverIP.text), int.Parse(serverPort.text));
         server = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
 
         string welcome = "Hello, are you there?";
