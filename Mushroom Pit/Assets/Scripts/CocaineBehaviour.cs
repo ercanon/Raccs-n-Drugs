01d7cad688653f3b63d9f085c90b49f248fda9b1
Alex Gesti - Pixel Cuak - Mr.Luigi001 <40181553+alexgesti@users.noreply.github.com>
2022-12-23 04:09:21 +0100

    More Feedback Racoon Charges

--- Mushroom Pit/Assets/Scripts/CocaineBehaviour.cs
+++ Mushroom Pit/Assets/Scripts/CocaineBehaviour.cs
@@ -8,40 +8,6 @@ public class CocaineBehaviour : MonoBehaviour
     public GameplayScript gameplayScript;
     public bool isBuffed = false;
 
-    // Buffed effect
-    MeshRenderer render;
-    [Range(0f, 1f)] public float transitionTime;
-    public Color[] colors;
-
-    int ColorIndex, len;
-    float t;
-
-    private void Start()
-    {
-        if (isBuffed)
-        {
-            render = this.gameObject.GetComponent<MeshRenderer>();
-            len = colors.Length;
-        }
-    }
-
-    private void Update()
-    {
-        if (isBuffed)
-        {
-            render.material.color = Color.Lerp(render.material.color, colors[ColorIndex], transitionTime * Time.deltaTime * 10);
-
-            t = Mathf.Lerp(t, 1f, transitionTime * Time.deltaTime * 10);
-
-            if (t > 0.9f)
-            {
-                t = 0;
-                ColorIndex++;
-                ColorIndex = (ColorIndex >= len) ? 0 : ColorIndex;
-            }
-        }
-    }
-
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
