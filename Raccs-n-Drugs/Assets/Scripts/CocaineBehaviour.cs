57982d539a6ed73f98452ba17f30fd9d4c989205
ercanon <61042236+ercanon@users.noreply.github.com>
2023-01-14 14:33:00 +0100

    Project Name Changed

---
+++ Raccs-n-Drugs/Assets/Scripts/CocaineBehaviour.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CocaineBehaviour : MonoBehaviour
+{
+    [HideInInspector]
+    public GameplayScript gameplayScript;
+    public bool isBuffed = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            if (isBuffed)
+            {
+                other.gameObject.GetComponent<RacoonBehaviour>().ChangeState(3);
+                gameplayScript.DeleteList();
+            }
+            else 
+            {
+                gameplayScript.UpdateList(this);
+                Destroy(gameObject);
+            }
+        }
+    }
+}
